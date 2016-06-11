a9c123c2fcf084ecf37a0acb0dd5480ffab817a2
Vitor Souza <root@php501.4linux.com.br>
2016-06-10 21:42:50 -0300

    banner

---
+++ src/js/main.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+
+	$('#galeria').slick({
+    	arrows: false,
+    	dots: true,
+    	autoplay: true,
+    	speedy: 2000,
+	});
+
+
+//  $('.slider-for').slick({
+//   slidesToShow: 1,
+//   slidesToScroll: 1,
+//   arrows: false,
+//   fade: true,
+//   asNavFor: '.slider-nav'
+// });
+
+});
