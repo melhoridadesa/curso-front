1f131874fcc47a72d374cc21318f8c832bf9cd79
Studios7Design <studios7design.com.br>
2016-06-27 08:55:27 -0300

    Ajustando formulário de contato com bootstrap e validação do nome, sobrenome e cpf / CEP fazendo a busca e jogando loading na tela, falta validar o CEP

---
+++ js/contato.js
@@ -0,0 +1,62 @@
+
+$(document).ready(function(){
+	//Mascaras
+	$("#cep").mask("99999-999");
+	$("#telefone").mask("(999) 9999-99999");
+	$('#cpf').mask("999.999.999-99");
+
+	//Buscar o CEP
+	$('#verificar_cep').click(function(){
+		var cep_para_busca = $('#cep').val();
+		$.ajax({
+			type :'GET',
+			url: "http://apps.widenet.com.br/busca-cep/api/cep/"+cep_para_busca+".json",
+			beforeSend: function() {
+				$('#loading').show();
+				//console.log('mandou');
+		    },
+			success: function(data) {
+				$('#rua').val(data.address);
+				$('#cep').val(data.code);
+				$('#bairro').val(data.district);
+				$('#cidade').val(data.city);
+				$('#estado').val(data.state);
+			},complete: function() {
+		        $('#loading').hide();
+		    }
+		});
+		return false;
+	});
+	
+	$('form').submit(function(){
+		$('input').removeClass('erro_class');
+		//Validacao CPF
+		var validar_cpf = /^([0-9]{3}\.[0-9]{3}\.[0-9]{3}\-[0-9]{2}|[0-9]{2}\.[0-9]{3}\.[0-9]{3}\/?[0-9]{4}\-?[0-9]{2})$/
+		cpf_valor = $('#cpf');
+		if (validar_cpf.test(cpf_valor.val()) == true) {
+			console.log('CPF Válido');
+		}else{
+			console.log("CPF Inválido");
+			cpf_valor.addClass('erro_class');
+		}
+		//Validacao nome
+		nome_valor = $('#nome').val();
+		if(nome_valor == "" || nome_valor.length < 3){
+			$('#nome').addClass('erro_class');
+			console.log($('#nome').length);
+		}	else{
+			console.log('nome correto');
+		}	
+		//Validacao sobrenome
+		sobrenome_valor = $('#sobrenome').val();
+		if(sobrenome_valor == "" || sobrenome_valor.length < 3){
+			$('#sobrenome').addClass('erro_class');
+			console.log($('#sobrenome').length);
+		}	else{
+			console.log('nome correto');
+		}	
+		return false;
+	});
+	//valida telefone
+
+});
